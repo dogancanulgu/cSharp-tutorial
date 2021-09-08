f848e41716f831382228c7602e75a0aa450f7600
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-09 02:24:17 +0300

    donguler while foreach

---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int sayi = int.Parse(Console.ReadLine());
+            // int sayac = 1;
+            // int toplam = 0;
+            // while(sayac <= sayi)
+            // {
+            //     toplam += sayac;
+            //     sayac++;
+            // }
+            // Console.WriteLine(toplam/sayi);
+            //****************************************
+            //     char character = 'a';
+            //     while(character <= 'z')
+            //     {
+            //         Console.Write(character);
+            //         character++;
+            //     }
+            // }
+            //*****************************************
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
