32d65b0438719b6dd82502436deb921faae0a9a5
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-09 00:53:42 +0300

    if-else-if

---
+++ if-else-if/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time >= 6 && time < 11)
+                System.Console.WriteLine("Günaydın");
+            if(time <= 10) 
+                System.Console.WriteLine("iyi günler");
+            else 
+                System.Console.WriteLine("iyi geceler");
+            
+            string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+            sonuc = time >= 6 && time < 11 ? "günaydın" : time <= 18 ? "iyi günler" : "iyi geceler";
+            System.Console.WriteLine(sonuc);
+        }
+    }
+}
